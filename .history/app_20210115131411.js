b4ef19455b8ce6128ded4c5f65fb3a6a06396d79
CAMPSLOPY <shobowaletola@gmail.com>
2021-01-15 13:15:13 +0100

    changes to the js file

--- .history/app_20210115131411.js
+++ .history/app_20210115131411.js
@@ -1,7 +1,7 @@
 const currencyEl_one = document.getElementById("currency-one");
 const currencyEl_two = document.getElementById("currency-two");
 const amountEl_one = document.getElementById("amount-one");
-const amountEl_two = document.getElementById("amount-two");
+const amountEl_two = document.getElementById("currency-one");
 const rateEl = document.getElementById("rate");
 const swap = document.getElementById("swap");
 
@@ -10,7 +10,7 @@ function calculate() {}
 
 calculate();
 
-// the event listeners
+
 currencyEl_one.addEventListener("change", calculate);
 amountEl_one.addEventListener("input", calculate);
 currencyEl_two.addEventListener("change", calculate);
